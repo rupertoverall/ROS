bd3f7c6bcc2ae58b9a17f44c1630f332df6fb7ab
Rupert Overall <mail@rupertoverall.net>
2019-01-28 10:17:48 +0100

    populating repo

---
+++ Figure_4/Figure_4.R
@@ -0,0 +1,134 @@
+source('http://research.rupertoverall.net/include.R')
+setwd('~/Dropbox/Projects/ROS/Paper Analysis/ROS Figures/')
+
+print(load("../SVZ.RData")) # "svz"
+print(load("../ROS.RData")) # "ros"
+
+# Annette's colours
+hiROS = "#FF6633"
+midROS = "#FF9933"
+loROS = "#CCCC33"
+green = "#5E9B3A"
+blue = "#3346A8"
+red = "#A70B02"
+annette.group.colours = c("+++" = hiROS, "++" = midROS, "+" = loROS)
+
+## PCA plot
+pca = PCA(t(ros$cpm))
+var.explained = round((pca$sdev)^2 / sum(pca$sdev^2) * 100)
+colour.factor = sapply(ros$Group, function(name) annette.group.colours[name] )
+pdf(file="Figure_4_Results/ROS QCPrincipalComponentPlots.pdf", width=5, height=5)
+plot(pca$PC1, pca$PC2, pch=20, cex=1.5, xlim=c(-130, 130), ylim=c(-130, 130), col=colour.factor, xlab=paste0("PC1 (", var.explained[1], " %)"), ylab=paste0("PC2 (", var.explained[2], " %)"))
+legend("topright", c("hiROS", "midROS", "loROS"), col=annette.group.colours, ncol=1, pch=19, lty=0, text.col="black")
+title(main="Principal component clustering of samples")
+dev.off()
+
+## Venn diagramme
+pdf(file="Figure_4_Results/ROS Venn.pdf", width=mm2in(85), height=mm2in(85))
+plot(0, type="n", bty="n", axes=F, xlab="", ylab="") # (Clear plot for stupid Venn code)
+# Enriched overlap
+VennDiagram::draw.triple.venn(
+  area1=length(ros$enriched$`+++`), 
+  area2=length(ros$enriched$`++`), 
+  area3=length(ros$enriched$`+`), 
+  n12=length(intersect(ros$enriched$`+++`, ros$enriched$`++`)), 
+  n23=length(intersect(ros$enriched$`++`, ros$enriched$`+`)), 
+  n13=length(intersect(ros$enriched$`+++`, ros$enriched$`+`)), 
+  n123=length(intersect(ros$enriched$`+++`, intersect(ros$enriched$`++`, ros$enriched$`+`))), 
+  category=names(ros$enriched), cex=0.7, cat.cex=0.7, cat.fontfamily=rep("sans", 3), fontfamily="sans", lty="blank", fill=annette.group.colours)
+dev.off()
+
+venn.numbers = function(){
+	hi=length(ros$enriched$`+++`) 
+	mid=length(ros$enriched$`++`) 
+	lo=length(ros$enriched$`+`)
+	hi.mid=length(intersect(ros$enriched$`+++`, ros$enriched$`++`))
+	lo.mid=length(intersect(ros$enriched$`++`, ros$enriched$`+`))
+	hi.lo=length(intersect(ros$enriched$`+++`, ros$enriched$`+`))
+	hi.mid.lo=length(intersect(ros$enriched$`+++`, intersect(ros$enriched$`++`, ros$enriched$`+`)))
+	
+	return(t(t(c(
+		hi.only = hi - (hi.mid + hi.lo + hi.mid.lo), 
+		mid.only = mid - (hi.mid + lo.mid + hi.mid.lo),
+		lo.only = mid - (hi.lo + lo.mid + hi.mid.lo),
+		hi.mid.only = hi.mid - hi.mid.lo,
+		lo.mid.only = lo.mid - hi.mid.lo,
+		hi.lo.only = hi.lo - hi.mid.lo,
+		hi.mid.lo.only = hi.mid.lo
+	))))
+}
+venn.numbers()
+
+## Functional annotation (run once)
+#$ source('http://research.rupertoverall.net/Signatures.R')
+#$ write.results.to.excel(ros$signatures, file="Figure_4_Results/SignatureEnrichment.xlsx")
+
+## Transcript overlap
+pdf("Figure_4_Results/SVZ overlaps.pdf", width=mm2in(85), height=mm2in(85))
+par(mfrow=c(1, 1), cex=0.5)
+intersections.ros = vcor(ros$signatures, svz$signatures, normalise=F)
+intersections.ros = intersections.ros / unlist(lapply(ros$signatures, length)) # Normalised to SVZ region totals
+barplot(intersections.ros, col=annette.group.colours, beside=TRUE, main="Overlap of genes of each ROS class in hippocampus regions")
+dev.off()
+
+## Cellular process markers
+# Hand-curated lists
+curated.markers = list(
+	cell.cycle.markers = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="cell_cycle_markers"), stringsAsFactors=F)[, "Ensembl.ID"]),
+	ng.markers = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="NG_markers"), stringsAsFactors=F)[, "Ensembl.ID"]),
+	cell.cycle = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="cell_cycle"), stringsAsFactors=F)[, "Ensembl.ID"]),
+	ng.trajectory = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="NG_trajectory"), stringsAsFactors=F)[, "Ensembl.ID"]),
+	ros.markers = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="ROS_markers"), stringsAsFactors=F)[, "Ensembl.ID"]),
+	other.cell.cycle = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="other_cell_cycle"), stringsAsFactors=F)[, "Ensembl.ID"]),
+	forkhead = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="forkhead"), stringsAsFactors=F)[, "Ensembl.ID"]),
+	tf.genes.q = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="TFs_waterfall_upinQ"), stringsAsFactors=F)[, "Ensembl.ID"]),
+	tf.genes.a = unique(data.frame(readxl::read_excel('../../Additional Data/Gene_lists.xlsx', sheet="TFs_waterfall_upinA"), stringsAsFactors=F)[, "Ensembl.ID"])
+)
+curated.markers.names = c(cell.cycle.markers="Cell cycle markers", ng.markers="Neurogenesis markers", cell.cycle="Cell cycle genes", ng.trajectory="Neurogenic trajectory", ros.markers="ROS_markers", other.cell.cycle="Other cell cycle genes", forkhead="Forkhead family", tf.genes.q="TFs_waterfall_upinQ", tf.genes.a="TFs_waterfall_upinA")
+curated.markers.pc1 = lapply(names(curated.markers), function(name) PCA(t(ros$cpm[intersect(rownames(ros$cpm), curated.markers[[name]]), ]))$PC1 )
+names(curated.markers.pc1) = names(curated.markers)
+#
+pdf("Figure_4_Results/Curated_lists.pdf", width=mm2in(85), height=mm2in(85))
+select.markers = curated.markers[c("ros.markers", "cell.cycle.markers", "ng.markers")]
+select.markers.pc1 = curated.markers.pc1[names(select.markers)]
+plot(1:length(levels(ros$Group)), type="n", ylim=range(do.call("rbind", select.markers.pc1)), ylab = "arb. unit")
+select.markers.colours = c(ros.markers=red, cell.cycle.markers=green, ng.markers=blue)
+for(name in names(select.markers)) lines(by(select.markers.pc1[[name]], ros$Group, mean), col=select.markers.colours[name], lwd=2, cex=.6)
+dev.off()
+
+## Transcription factors from Shin et al.
+pdf("Figure_4_Results/TF_lists.pdf", width=mm2in(85), height=mm2in(85))
+tf.markers = curated.markers[grep("tf\\.genes", names(curated.markers))]
+tf.markers.pc1 = curated.markers.pc1[names(tf.markers)]
+plot(1:length(levels(ros$Group)), type="n", ylim=range(do.call("rbind", tf.markers.pc1)), ylab = "arb. unit")
+tf.markers.colours = c(tf.genes.q=red, tf.genes.a=green)
+for(name in names(tf.markers)) lines(by(tf.markers.pc1[[name]], ros$Group, mean), col=tf.markers.colours[name], lwd=2, cex=.6)
+dev.off()
+
+## Expression in Shin et al.
+print(load("~/Dropbox/Work/Resources/Shin_Waterfall/Shin.RData")) # Shin et al. with pseudotime correction
+# ROS clusters projected onto Shin
+shin.mapping = AnnotationDbi::select(org.Mm.eg.db::org.Mm.eg.db, keys=rownames(shin$pseudotime$expr), columns=c("ENSEMBL"), "SYMBOL")
+shin.mapping = shin.mapping[match(rownames(shin$pseudotime$expr), shin.mapping$SYMBOL), ]
+shin.mapping = shin.mapping[!is.na(shin.mapping$ENSEMBL), ]
+shin.common = shin$pseudotime$expr[shin.mapping$SYMBOL, ]
+#
+ros.pc1 = lapply(levels(ros$Group), function(group){
+	common = shin.mapping$ENSEMBL %in% ros$signatures[[group]]
+	data = t(shin$pseudotime$expr[common, ])
+	data = data[, which(colVars(data) > 0)]
+	print(ncol(data))
+	PCA(data)$PC1
+})
+names(ros.pc1) = levels(ros$Group)
+#
+pdf(paste0("Figure_4_Results/Pseudotime.pdf"), width=mm2in(85), height=mm2in(85))
+plot(NA, xlim=c(0, 1), ylim=c(-8, 8), type="n", main="Expression in Shin et al.", ylab="PC1 expression\n(smooth spline interpolation)", xlab="pseudotime")
+for(group in levels(ros$Group)){
+	smooth = smooth.spline(shin$pseudotime$x, ros.pc1[[group]], spar=0.7)
+	lines(smooth, col=annette.group.colours[[group]], lwd=2)
+}
+dev.off()
+
+writeLines(capture.output(sessionInfo()), "Figure_4_Results/sessionInfo.txt")
+##########
---
+++ Signatures.R
@@ -0,0 +1,178 @@
+source('http://research.rupertoverall.net/include.R')
+
+require(edgeR)
+require(XLConnect)
+require(topGO)
+require(ReactomePA)
+
+calculate.signatures = function(counts, groups, p=0.05, top=NA, filter=NA){
+  # Counts must be raw reads
+  counts = as.matrix(counts)
+  if(!is.factor(groups)) groups = as.factor(groups) 
+  # The parameter 'filter' MUST be provided (T/F) [to protect against old code which should fail if not aware of this option]
+  if(is.na(filter)){ stop("The logical parameter 'filter' is missing")}
+  else if(filter){
+    # Keep only features which have more than 1 cpm in at least 3/4 of the samples for at least one group
+    present = cpm.presence.filter(counts, groups, nGroupsToPass=1, fractionSamplesPerGroupToPass=0.75, threshold=1)
+    counts = counts[present, ]
+  }
+  cpm = cpm(counts, prior.count=1, log=T)
+  
+  # Filter to select genes that are regulated at all
+  design = model.matrix(~groups)
+  fit = glmFit(estimateDisp(calcNormFactors(DGEList(counts=counts, group=groups)), design), design)
+  top.tags = NULL
+  if(!is.na(top)){
+  	top.tags = topTags(glmLRT(fit, coef=2:length(levels(groups))), n=top, p.value=1)$table
+  }else{
+  	top.tags = topTags(glmLRT(fit, coef=2:length(levels(groups))), n=nrow(counts), p.value=p)$table
+  }
+  
+  # Enriched if _significantly_ greater than mean
+  # The idea here is to protect against cases where most of the groups have high expression and only one is low. Such genes would seem to be poor candidates for being classed as 'enriched' in all the high celltypes.
+  all.means = rowMeans(cpm[rownames(top.tags), ])
+  enriched = lapply(levels(groups), function(group){
+    group.indices = which(groups==group)
+    tests = sapply(rownames(top.tags), function(i){
+      if(var(cpm[i, group.indices]-all.means[i])<1e-20) 1 else unname(t.test(cpm[i, group.indices], mu=all.means[i], alternative="greater")$p.value)
+    })
+    names(which(tests < 0.05))
+  })
+  names(enriched) = levels(groups)
+
+  # Signature gene if both enriched AND in a cluster of its own
+  # Clusters are calculated by absolute t-statistic distances between groups
+  signatures = lapply(levels(groups), function(group){
+		is.signature = sapply(enriched[[group]], function(v){
+			tmatrix = tcor(cpm[v, ], groups) # tcor() from include()
+			tmatrix[is.na(tmatrix)] = 0 # Try to catch invariant groups (i.e. non-expressed)
+			clusters = cutree(hclust(as.dist(abs(tmatrix))), k=2)
+			cluster.sizes = sapply(clusters,function(cluster) length(which(clusters==cluster)) )
+			cluster.sizes[group] == 1 # The only one in its group?
+		})
+		enriched[[group]][unlist(is.signature)]
+  })
+  names(signatures) = levels(groups)
+
+  return(
+    list("enriched"=enriched, "signatures"=signatures, "top.tags"=top.tags, "cpm"=cpm)
+  )
+}
+
+collapse.replicate.names = function(rowname.ids){
+  # When converting between IDs or species, 1:many mapping means there are replicate rownames - identified by trailing subindices
+  # This function strips the vector of names and just returns the unique base ids
+  stripped.ids = unique(sub("\\..*", "", rowname.ids))
+  return(stripped.ids)
+}
+
+clean.replicate.names = function(rowname.ids, collapse=T){
+  # When converting between IDs or species, 1:many mapping means there are replicate rownames - identified by trailing subindices
+  # This function strips the vector of names and just returns the unique base ids
+  stripped.ids = sub("\\..*", "", rowname.ids)
+  if(collapse) stripped.ids = unique(stripped.ids)
+  return(stripped.ids)
+}
+
+write.results.to.excel = function(input, file, id="ENSEMBL", background=NA, species="mouse"){
+  # input is a list of data.frames (or matrices)
+  # The first column must be a gene ID. 
+  # Remaining columns will be written as metadata columns
+  # 'id' is the geneid used. Either "ENSEMBL" or "ENTREZID"
+  # 'species is either "mouse" or "human".
+  group.names = names(input)
+  workbook = XLConnect::loadWorkbook(file, create=T)
+  XLConnect::createSheet(workbook, group.names) #Create empty sheets (which will be overwritten) and...
+  XLConnect::removeSheet(workbook, setdiff(XLConnect::getSheets(workbook), group.names)) # ...delete other existing sheets.
+  db = NULL
+  if(species=="mouse") {require(org.Mm.eg.db); db = org.Mm.eg.db}
+  if(species=="human") {require(org.Hs.eg.db); db = org.Hs.eg.db}
+  if(id=="ENSEMBL"){
+    # Gene lists
+    . = lapply(group.names, function(group){
+      XLConnect::createSheet(workbook, group)
+      XLConnect::clearSheet(workbook, group)
+      data = select(db, keys=as.matrix(input[[group]])[, 1], columns=c("SYMBOL", "ENTREZID"), "ENSEMBL")
+      meta = as.data.frame(input[[group]])
+      colnames(meta) = colnames(input[[group]])
+      data = merge(data, meta, by=1)
+      XLConnect::writeWorksheet(workbook, data, group)
+      XLConnect::setColumnWidth(workbook, group, 1:ncol(data), -1)
+    })
+    # GO
+    if(length(background)<2) background = keys(db, keytype="ENSEMBL")
+    background.genes = select(db, keys=background, columns=c("SYMBOL", "ENTREZID"), "ENSEMBL")
+    background.ENSEMBL = unique(background.genes$ENSEMBL[!is.na(background.genes$ENSEMBL)])
+    go.results = lapply(group.names, function(group){
+      go = GOForIt(background.ENSEMBL, collapse.replicate.names(as.matrix(input[[group]])[, 1]), id.type='ensembl', species=species)
+    })
+    names(go.results) = group.names
+    . = lapply(group.names, function(group){
+      XLConnect::createSheet(workbook, paste0(group, "_GO"))
+      XLConnect::clearSheet(workbook, paste0(group, "_GO"))
+      data = go.results[[group]]$result.table
+      XLConnect::writeWorksheet(workbook, data, paste0(group, "_GO"))
+      XLConnect::setColumnWidth(workbook, paste0(group, "_GO"), 1:ncol(data), -1)
+    })
+    # Reactome
+    background.ENTREZID = unique(background.genes$ENTREZID[!is.na(background.genes$ENTREZID)])
+    reactome.results = sapply(group.names, function(group){
+      query = select(db, keys=collapse.replicate.names(as.matrix(input[[group]])[, 1]), columns=c("ENTREZID"), "ENSEMBL")
+      query = unique(query$ENTREZID[!is.na(query$ENTREZID)])
+      reactome.results = enrichPathway(query, universe=background.ENTREZID, organism="mouse", pvalueCutoff=0.05, pAdjustMethod = "BH")
+    })
+    names(reactome.results) = group.names
+    . = lapply(group.names, function(group){
+      XLConnect::createSheet(workbook, paste0(group, "_Reactome"))
+      XLConnect::clearSheet(workbook, paste0(group, "_Reactome"))
+      data = as.data.frame(reactome.results[[group]])
+      XLConnect::writeWorksheet(workbook, data, paste0(group, "_Reactome"))
+      XLConnect::setColumnWidth(workbook, paste0(group, "_Reactome"), 1:ncol(data), -1)
+    })
+    XLConnect::saveWorkbook(workbook)
+  }
+  if(id=="ENTREZID"){
+    # Gene lists
+    . = lapply(group.names, function(group){
+      createSheet(workbook, group)
+      clearSheet(workbook, group)
+      data = select(db, keys=as.matrix(input[[group]])[, 1], columns=c("SYMBOL", "ENSEMBL"), "ENTREZID")
+      meta = as.data.frame(input[[group]])
+      colnames(meta) = colnames(input[[group]])
+      data = merge(data, meta, by=1)
+      writeWorksheet(workbook, data, group)
+      setColumnWidth(workbook, group, 1:ncol(data), -1)
+    })
+    # GO
+    if(length(background)<2) background = keys(db, keytype="ENTREZID")
+    background.ENTREZID = unique(background[!is.na(background)])
+    go.results = lapply(group.names, function(group){
+      go = GOForIt(background.ENTREZID, collapse.replicate.names(as.matrix(input[[group]])[, 1]), id.type='entrez', species=species)
+    })
+    names(go.results) = group.names
+    . = lapply(group.names, function(group){
+      createSheet(workbook, paste0(group, "_GO"))
+      clearSheet(workbook, paste0(group, "_GO"))
+      data = go.results[[group]]$result.table
+      writeWorksheet(workbook, data, paste0(group, "_GO"))
+      setColumnWidth(workbook, paste0(group, "_GO"), 1:ncol(data), -1)
+    })
+    # Reactome
+    reactome.results = sapply(group.names, function(group){
+      query = collapse.replicate.names(as.matrix(input[[group]])[, 1])
+      query = unique(query[!is.na(query)])
+      reactome.results = enrichPathway(query, universe=background, organism="mouse", pvalueCutoff=0.05, pAdjustMethod = "BH")
+    })
+    names(reactome.results) = group.names
+    . = lapply(group.names, function(group){
+      createSheet(workbook, paste0(group, "_Reactome"))
+      clearSheet(workbook, paste0(group, "_Reactome"))
+      data = as.data.frame(reactome.results[[group]])
+      writeWorksheet(workbook, data, paste0(group, "_Reactome"))
+      setColumnWidth(workbook, paste0(group, "_Reactome"), 1:ncol(data), -1)
+    })
+    saveWorkbook(workbook)
+  }
+}
+
+##########
